c2af0232f1d2857cb290f363a2bb53d06fe5b967
DidacusZ <127140589+DidacusZ@users.noreply.github.com>
2023-06-07 11:28:41 +0200

    Update gigante-ssh-bot.py

--- gigante-ssh-bot.py
+++ gigante-ssh-bot.py
@@ -79,7 +79,7 @@ def check_sshBF(ip):
 		try:
 			print colores.verde + "|----[INFO] Try user: root password: " + p + colores.normal
 			ssh.connect(ip, username=user, password=p, timeout=10)
-			print p + " es la contraseña correcta"
+			print "contraseña correcta"
 		except paramiko.AuthenticationException, error:
 			print "|!----[ERROR] incorrect password... root@" + ip + ":" + p
 			continue
